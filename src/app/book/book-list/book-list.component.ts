52925f358d2769c7ee59f3a67100a7d5567bd561
Dave <dave@webdave.de>
2024-05-07 12:45:09 +0200

    Signals

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,21 +1,17 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Signal, inject } from '@angular/core';
 import { BookApiService } from '../book-api.service';
 import { Book } from '../models';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { RouterLink } from '@angular/router';
 import { NgFor, NgIf, AsyncPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'ws-book-list',
-    templateUrl: 'book-list.component.html',
-    standalone: true,
-    imports: [NgFor, NgIf, RouterLink, BookCardComponent, AsyncPipe]
+  selector: 'ws-book-list',
+  templateUrl: 'book-list.component.html',
+  standalone: true,
+  imports: [NgFor, NgIf, RouterLink, BookCardComponent, AsyncPipe]
 })
 export class BookListComponent {
-  books$: Observable<Book[]>;
-
-  constructor(private bookData: BookApiService) {
-    this.books$ = this.bookData.getAll();
-  }
+  books: Signal<Book[] | undefined> = toSignal(inject(BookApiService).getAll());
 }
