b234d834688b08bf15c707f7b431603bd6db4954
Dave <dave@webdave.de>
2024-05-08 14:03:08 +0200

    Hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule, provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
@@ -11,6 +11,7 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(BrowserModule, FormsModule, HammerModule),
     provideHttpClient(withInterceptorsFromDi(), withFetch()),
     provideAnimations(),
-    provideRouter(appRoutes, withComponentInputBinding())
+    provideRouter(appRoutes, withComponentInputBinding()),
+    provideClientHydration()
   ]
 };
