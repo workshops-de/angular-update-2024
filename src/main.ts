527b149e5fec4d9f559313cd3085d8d85d99dbb0
Dave <dave@webdave.de>
2024-05-15 10:14:35 +0200

    clean up

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule, HammerModule, bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
@@ -14,10 +12,9 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, FormsModule, HammerModule, AppRoutingModule),
-        provideHttpClient(withInterceptorsFromDi()),
-        provideAnimations()
-    ]
-})
-  .catch(err => console.log(err));
+  providers: [
+    importProvidersFrom(BrowserModule, HammerModule, AppRoutingModule),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations()
+  ]
+}).catch(err => console.log(err));
