4aa591bf99b282ea38d6ad2aeab1be8d27edfa84
Dave <dave@webdave.de>
2024-05-16 12:06:22 +0200

    signal

--- src/app/book/book-detail/book-detail.component.ts
+++ src/app/book/book-detail/book-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, Input, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, Input, OnInit, effect, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { NEVER, Observable } from 'rxjs';
@@ -17,11 +17,19 @@ export class BookDetailComponent implements OnInit {
   @Input() isbn = '';
   public book$: Observable<Book> = NEVER;
 
+  resize = signal(true);
+
   private router = inject(Router);
   private bookService = inject(BookApiService);
   private destroyRef = inject(DestroyRef);
   ngOnInit(): void {
+    const e = effect(() => {
+      if (this.resize()) {
+      }
+    });
+
     this.book$ = this.bookService.getByIsbn(this.isbn);
+    e.destroy();
   }
 
   remove() {
--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,17 +1,18 @@
-import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Signal, inject } from '@angular/core';
 import { BookApiService } from '../book-api.service';
 import { Book } from '../models';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { RouterLink } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'ws-book-list',
   templateUrl: 'book-list.component.html',
   standalone: true,
-  imports: [RouterLink, BookCardComponent, AsyncPipe]
+  imports: [RouterLink, BookCardComponent]
 })
 export class BookListComponent {
-  books$: Observable<Book[]> = inject(BookApiService).getAll();
+  books: Signal<Book[]> = toSignal(inject(BookApiService).getAll(), {
+    initialValue: []
+  });
 }
