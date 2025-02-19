b39f890416daeffcc5df68058a356cf3bd8ccf5c
Dave <dave@webdave.de>
2025-02-19 12:12:41 +0100

    input signal

--- src/app/book/book-detail/book-detail.component.ts
+++ src/app/book/book-detail/book-detail.component.ts
@@ -1,26 +1,31 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, computed, inject, input } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { NEVER, Observable } from 'rxjs';
 import { exhaustMap, switchMap, tap } from 'rxjs/operators';
 import { BookApiService } from '../book-api.service';
 import { Book } from '../models';
 import { AsyncPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({ selector: 'ws-book-detail', templateUrl: 'book-detail.component.html', imports: [RouterLink, AsyncPipe] })
-export class BookDetailComponent implements OnInit {
+export class BookDetailComponent implements OnInit, OnChanges {
   private router = inject(Router);
   private bookService = inject(BookApiService);
 
   public book$: Observable<Book> = NEVER;
-  @Input({ required: true }) isbn!: string;
+  isbn = input.required<string>();
+  // book = computed(() => {
+  //   const isbn = this.isbn();
+  //   return toSignal<Book>(this.bookService.getByIsbn(isbn));
+  // });
 
-  ngOnInit(): void {
-    this.book$ = this.bookService.getByIsbn(this.isbn);
+  ngOnChanges(): void {
+    this.book$ = this.bookService.getByIsbn(this.isbn());
   }
 
   remove() {
     this.bookService
-      .delete(this.isbn)
+      .delete(this.isbn())
       .pipe(tap(() => this.router.navigateByUrl('/')))
       .subscribe();
   }
